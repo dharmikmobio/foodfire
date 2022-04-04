f79dd127c68c5983eb190f90cb25a3cf7271cfe4
Dharmik <dharmikp.mobio@gmail.com>
2022-04-04 14:32:41 +0530

    three

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import React ,{useContext} from 'react';
+import { auth } from '../fireConfig';
+
+const AuthContext = React.createContext();
+export function AuthProvider({children}){
+  return(
+    <>
+      <AuthContext.Provider value={value}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  )
+}
+
+export function useAuth(){
+  return useContext(AuthContext);
+}
+const resetPassword = (email) =>{
+  return auth.sendPasswordResetEmail(email);
+}
+
+const value = {
+  resetPassword,
+};
